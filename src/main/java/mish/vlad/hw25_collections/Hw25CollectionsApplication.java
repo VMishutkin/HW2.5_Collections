123afe32cc023b4479488ceb57929babde42e72b
VMishutkin <vladmish@list.ru>
2022-08-31 10:30:39 +0700

    Initial commit

---
+++ src/main/java/mish/vlad/hw25_collections/Hw25CollectionsApplication.java
@@ -0,0 +1,13 @@
+package mish.vlad.hw25_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw25CollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw25CollectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mish/vlad/hw25_collections/Hw25CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package mish.vlad.hw25_collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw25CollectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
